3ec0b7a1401a726faf99748eb0c8d6a08860123b
Anand Agarwal <anand20agarwal00@gmail.com>
2025-03-05 21:40:51 +0530

    updates

--- client/src/components/admin-view/header.jsx
+++ client/src/components/admin-view/header.jsx
@@ -18,7 +18,7 @@ function AdminHeader({ setOpen }) {
       </Button>
       <div className="flex flex-1 justify-end">
         <Button
-          onClick={handleLogout}
+          onClick={handleLogout}      
           className="inline-flex gap-2 items-center rounded-md px-4 py-2 text-sm font-medium shadow"
         >
           <LogOut />            
